b8101eaf732ffffdb93c20ec0c1566bc478e3d62
Lac Akkarapon <lacakkarapon@gmail.com>
2021-06-20 11:55:13 +0700

    EP11
    - สินค้าชนิดเดียวกันรวมกันก่อน
    - edit order
    - delete order, alert
    - badge

---
+++ Django50Hrs/EP11/firstweb/myapp/urls.py
@@ -0,0 +1,16 @@
+# urls.py
+
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+	path('', Home, name='home-page'),
+	path('about/', About, name='about-page' ),
+	path('contact/', Contact, name='contact-page'),
+	path('apple/', Apple, name='apple-page'),
+	path('addproduct/', AddProduct, name='addproduct-page'),
+	path('allproduct/', Product, name='allproduct-page'),
+	path('register/', Register, name='register-page'),
+	path('addtocart/<int:pid>/', AddtoCart, name='addtocart-page'),
+	path('mycart/', MyCart, name='mycart-page')
+]
